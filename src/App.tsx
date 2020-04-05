e5b087d6c83d479779ea6cbb622df5402e3bfbe4
frankfang <frankfang1990@gmail.com>
2020-04-05 22:23:26 +0800

    添加 svg 文件

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -16,17 +17,6 @@ const Wrapper = styled.div`
 const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
-`;
-const Nav = styled.nav`
-  border: 1px solid blue;
-  > ul {
-    display:flex;
-    > li{
-      width: 33.3333%;
-      text-align:center;
-      padding: 16px;
-    }
-  }
 `;
 
 function App() {
@@ -50,19 +40,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
---
+++ src/components/Nav.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  > ul {
+    display:flex;
+    > li{
+      width: 33.3333%;
+      text-align:center;
+      padding: 16px;
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <Link to="/money">记账页</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
