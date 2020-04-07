7b3015f47c8fcf12ef8480aa53ff17d822af35d0
frankfang <frankfang1990@gmail.com>
2020-04-07 22:23:00 +0800

    创建 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
+import Icon from './Icon';
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -32,21 +29,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#tag"/>
-          </svg>
+          <Icon name="tag"/>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money"/>
-          </svg>
+          <Icon name="money"/>
           <Link to="/money">记账页</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#chart"/>
-          </svg>
+          <Icon name="chart"/>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
