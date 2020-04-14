b44be427e77ffe290dc084b65e09d45d2af6a7d1
frankfang <frankfang1990@gmail.com>
2020-04-14 22:46:08 +0800

    抽离 useTags 函数

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
+import {useTags} from 'useTags';
 
 const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
@@ -26,7 +27,7 @@ type Props = {
   onChange: (selected: string[]) => void;
 }
 const TagsSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const {tags, setTags} = useTags();
   const selectedTags = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('新标签的名称为');
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,16 @@
 import Layout from '../components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
+import {useTags} from 'useTags';
 
 function Tags() {
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag}>{tag}</li>
+        )}
+      </ol>
     </Layout>
   );
 }
