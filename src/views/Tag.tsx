0bed58b6dd86c8b8a66a38091b6917a4a1423037
frankfang <frankfang1990@gmail.com>
2020-04-16 11:12:50 +0800

    封装 findTag 到 useTags 里

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
 
-const Tag: React.FC = () => {
+type Params = {
+  id: string
+}
+const Tag: React.FC = (props) => {
+  const {findTag} = useTags();
+  let {id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
   return (
-    <div>hi</div>
+    <div>{tag.name}</div>
   );
 };
 
